4073ba8f27ce86901bed2a9788d582c027673042
Chinseok Lee <me@askcompany.kr>
2020-10-17 14:18:09 +0900

    catube 앱 생성 및 Video 모델 생성 및 admin 등록

---
+++ step1-django-project/dj_proj/catube/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+
+# 생성될 테이블명 : catube_video
+class Video(models.Model):
+    author_name = models.CharField(max_length=20)
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    file = models.FileField()
+    photo = models.ImageField()
+    view_count = models.PositiveIntegerField(default=0)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
